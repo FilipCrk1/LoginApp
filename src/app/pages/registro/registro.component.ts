40e3eec6ae9f227845f2c8e458fd102079bfeaf1
Felipe Colin <leonel.pipe82@gmail.com>
2020-11-14 12:36:51 -0600

    Mostrar Notificaciones al usuario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsuarioModel } from '../../models/usuario.model';
-
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
@@ -12,7 +13,8 @@ export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
 
-  constructor( private auth: AuthService) { }
+  constructor( private auth: AuthService,
+    private router: Router ) { }
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
@@ -20,12 +22,24 @@ export class RegistroComponent implements OnInit {
 
   onSubmit( form: NgForm) {
     if ( form.invalid ) { return; }
-
+    Swal.fire({
+      allowOutsideClick: false,
+      type: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
     this.auth.nuevoUsuario( this.usuario )
     .subscribe( resp => {
       console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
+      Swal.fire({
+        type: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
     }
     );
   }
