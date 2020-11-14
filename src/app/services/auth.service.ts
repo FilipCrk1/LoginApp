3f8313ff0e5d30f3a54ca3bbed97dd26a988b075
Felipe Colin <leonel.pipe82@gmail.com>
2020-11-14 08:10:55 -0600

    Se pone api y codigo para contraseña y email

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UsuarioModel } from '../models/usuario.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+    private url = 'https://identitytoolkit.googleapis.com/v1';
+    private apikey = ' AIzaSyBJYluKWtrF5LeRUP98B0bSETfpJh0Xk_E';
+  //Crear Nuevo usuario
+  //https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+  //Login
+  //https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
+  constructor( private http: HttpClient ) { }
+
+  logout() {
+
+  }
+  login( usuario: UsuarioModel ) {
+
+  }
+  nuevoUsuario( usuario: UsuarioModel ) {
+
+  }
+}
