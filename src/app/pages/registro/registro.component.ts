4765ef6ea5211d14a38517f796804e21cb873b69
Felipe Colin <leonel.pipe82@gmail.com>
2020-11-21 12:43:38 -0600

    Recordar Usuario

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsuarioModel } from '../../models/usuario.model';
-import Swal from 'sweetalert2';
+//import Swal from 'sweetalert2';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-registro',
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 export class RegistroComponent implements OnInit {
 
   usuario: UsuarioModel;
-
+  recordarme = false;
   constructor( private auth: AuthService,
     private router: Router ) { }
 
@@ -22,26 +22,30 @@ export class RegistroComponent implements OnInit {
 
   onSubmit( form: NgForm) {
     if ( form.invalid ) { return; }
-    Swal.fire({
+    /*Swal.fire({
       allowOutsideClick: false,
       type: 'info',
       text: 'Espere por favor...'
     });
-    Swal.showLoading();
+    Swal.showLoading();*/
     this.auth.nuevoUsuario( this.usuario )
     .subscribe( resp => {
       console.log(resp);
-      Swal.close();
+      //Swal.close();
+      if ( this.recordarme ) {
+        localStorage.setItem('email', this.usuario.email);
+      }
       this.router.navigateByUrl('/home');
     }, (err) => {
       console.log(err.error.error.message);
-      Swal.fire({
+     /* Swal.fire({
         type: 'error',
         title: 'Error al autenticar',
         text: err.error.error.message
       });
-    }
-    );
-  }
+    }*/
 
+  });
+
+}
 }
